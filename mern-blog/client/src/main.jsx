36b17d7c55c1787c0282c32c275279c9769b6bd4
audie38 <milson.audie@gmail.com>
2024-02-10 15:43:52 +0700

    Init MERN Blog Project

---
+++ mern-blog/client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import "./assets/css/fontawesome.css";
+import "./assets/css/bootstrap.css";
+import "./assets/css/style.css";
+import "./assets/js/bootstrap.bundle.min.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
