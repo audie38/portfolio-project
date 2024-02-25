d3c38d4c9c389206c240b5e2ef4719c1f30b67fc
audie38 <milson.audie@gmail.com>
2024-02-25 21:17:51 +0700

    Next Js Client Components & Router Hooks

--- next-property/app/properties/[id]/page.jsx
+++ next-property/app/properties/[id]/page.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
 
 const PropertyDynamic = () => {
   // [id] used for Dynamic URL
   // To Add Catch All Dynamic URL Path Rename the folder name to [...id]
-  return <div>Property Dynamic</div>;
+  const router = useRouter();
+  const { id } = useParams(); // get url parameter value
+  const searchParams = useSearchParams(); // get url query parameter
+  const name = searchParams.get("name");
+  const pathName = usePathname(); // get url path
+
+  return (
+    <div>
+      <button className="bg-blue-500 p-2" onClick={() => router.push("/")}>
+        Go Home From {pathName} - {name} - {id}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyDynamic;
