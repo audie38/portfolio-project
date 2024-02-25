c6916bd657369da2c2fc8f49bb7e6eabfaf2fe38
audie38 <milson.audie@gmail.com>
2024-02-25 20:41:35 +0700

    Next Js Layout, page & metadata

---
+++ next-property/app/layout.jsx
@@ -0,0 +1,19 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "Next Property | Find The Perfect Property",
+  description: "Find your dream property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
---
+++ next-property/app/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Next Property | Home Page",
+};
+
+const page = () => {
+  return <div>Home Page</div>;
+};
+
+export default page;
