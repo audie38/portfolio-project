16d12efd3f8f6de485e21366b17c414663d2452d
audie38 <milson.audie@gmail.com>
2024-03-04 19:17:29 +0700

    Add Home page & Footer component

--- next-property/app/page.jsx
+++ next-property/app/page.jsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 export const metadata = {
   title: "Next Property | Home Page",
@@ -6,10 +7,10 @@ export const metadata = {
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Show Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   );
 };
 
--- next-property/components/Navbar.jsx
+++ next-property/components/Navbar.jsx
@@ -45,8 +45,8 @@ const Navbar = () => {
 
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <Link className="flex flex-shrink-0 items-center" href="/">
-              <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">PropertyPulse</span>
+              <Image className="h-10 w-auto" src={logo} alt="NextProperty" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">NextProperty</span>
             </Link>
 
             <div className="hidden md:ml-6 md:block">
